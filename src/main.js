bfe26ffdf6f442ec6581e7a8177b7e1ddeb1ffa5
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2023-06-03 14:02:59 +0200

    Commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// const indentedArticle = document.querySelector(".article-indented .container");
+// const shortenerWrapHeight = document.querySelector(".shortener-wrap").offsetHeight; 
+
+// indentedArticle.style.top = `-${shortenerWrapHeight / 2}px`
